0b154abfdd821783968ee9accfc99d17ef6bf64c
sub <htcworx@gmail.com>
2022-01-17 23:22:27 +0100

    add first couple values

--- persona-5-royal/persona-5-royal.js
+++ persona-5-royal/persona-5-royal.js
@@ -10,30 +10,16 @@ SavegameEditor={
 
 	Offsets:{
 		MONEY:0x357c,
-		PROTAGONISTXP:0x6c,
-		PROTAGONISTSTATS:0x6c,
+		HEROXP:0x6c,
+		HEROHP:0x5c,
+		HEROSP:0x60,
 		RYUJIXP:0x344,
+		MORGANAXP:0x5ec,
+		ANNXP:0x894,
+		YUSUKEXP:0xb3c,
 		BOUGHTPICRITES:0x0b44,
 		PENDINGPICRITES:0x0b48, /* if there is an error after buying, they are queued here */
 		UNLOCKEDSHOPFLAG:0x0b4c,
-	},
-
-	Constants:{
-		PROTAGONISTSTATS:[
-			'XP',
-			'HP',
-			'SP',
-			'Courage',
-			'Knowledge',
-			'Expression',
-			'Understanding',
-			'Diligence'
-		],
-		CHARACTERSTATS:[
-			'XP',
-			'HP',
-			'SP'
-		]
 	},
 
 	/* check if savegame is valid */
@@ -52,36 +38,29 @@ SavegameEditor={
 		tempFile.fileName='DATA.DAT';
 
 		setValue('money', tempFile.readU32(this.Offsets.MONEY));
+		setValue('heroxp', tempFile.readU32(this.Offsets.HEROXP));
+		setValue('herohp', tempFile.readU32(this.Offsets.HEROHP));
+		setValue('herosp', tempFile.readU32(this.Offsets.HEROSP));
+		setValue('ryujixp', tempFile.readU32(this.Offsets.RYUJIXP));
+		setValue('morganaxp', tempFile.readU32(this.Offsets.MORGANAXP));
+		setValue('annxp', tempFile.readU32(this.Offsets.ANNXP));
+		setValue('yusukexp', tempFile.readU32(this.Offsets.YUSUKEXP));
 		setValue('boughtpicrites', tempFile.readU16(this.Offsets.BOUGHTPICRITES));
-
-		/* PROTAGONISTSTATS */
-		for(var i=0; i<this.Constants.PROTAGONISTSTATS.length; i++){
-			var val,input;
-			/*if(i==24 || i==25){
-				val=tempFile.readF32(this.Offsets.PROTAGONISTSTATS+i*4);
-				//console.log((this.Offsets.STATS+i*4).toString(16));
-				input=inputFloat('stat'+i, 0.0, 9.0, val);
-			}else{ */
-				val=tempFile.readU32(this.Offsets.PROTAGONISTSTATS+i*4);
-				input=inputNumber('protagoniststat'+i, 0, 0xffffffff, val);
-			//}
-
-			document.getElementById('protagoniststats').appendChild(
-				row(
-					[8,4],
-					label('protagoniststat'+i, this.Constants.PROTAGONISTSTATS[i]),
-					input
-				)
-			);
-		}
 	},
 
 	/* save function */
 	save:function(){
 		tempFile.writeU32(this.Offsets.MONEY, getValue('money'));
-		var boughtPicrites=getValue('boughtpicrites');
-		tempFile.writeU16(this.Offsets.BOUGHTPICRITES, boughtPicrites);
-		var unlockedShopByte=tempFile.readU8(this.Offsets.UNLOCKEDSHOPFLAG) & ~0x01;
-		tempFile.writeU8(this.Offsets.UNLOCKEDSHOPFLAG, unlockedShopByte+(boughtPicrites>=5000)?0x01:0x00);
+		tempFile.writeU32(this.Offsets.HEROXP, getValue('heroxp'));
+		tempFile.writeU32(this.Offsets.HEROHP, getValue('herohp'));
+		tempFile.writeU32(this.Offsets.HEROSP, getValue('herosp'));
+		tempFile.writeU32(this.Offsets.RYUJIXP, getValue('ryujixp'));
+		tempFile.writeU32(this.Offsets.MORGANAXP, getValue('morganaxp'));
+		tempFile.writeU32(this.Offsets.ANNXP, getValue('annxp'));
+		tempFile.writeU32(this.Offsets.YUSUKEXP, getValue('yusukexp'));
+		/*var boughtPicrites=getValue('boughtpicrites');
+		tempFile.writeU16(this.Offsets.BOUGHTPICRITES, boughtPicrites);*/
+		/*var unlockedShopByte=tempFile.readU8(this.Offsets.UNLOCKEDSHOPFLAG) & ~0x01;
+		tempFile.writeU8(this.Offsets.UNLOCKEDSHOPFLAG, unlockedShopByte+(boughtPicrites>=5000)?0x01:0x00);*/
 	}
 }
