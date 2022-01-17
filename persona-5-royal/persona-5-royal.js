0db9ac7eff3d594387340d0a76d09a0faca82fac
sub <htcworx@gmail.com>
2022-01-17 15:59:51 +0100

    first test (money)

---
+++ persona-5-royal/persona-5-royal.js
@@ -0,0 +1,45 @@
+/*
+	Persona 5 Royal for HTML5 Save Editor v20220117
+	by Marc Robledo 2016-2017
+	subworx 2022
+*/
+
+SavegameEditor={
+	Name:'Persona 5 Royal',
+	Filename:'DATA.DAT',
+
+	Offsets:{
+		MONEY:0x357c,
+		BOUGHTPICRITES:0x0b44,
+		PENDINGPICRITES:0x0b48, /* if there is an error after buying, they are queued here */
+		UNLOCKEDSHOPFLAG:0x0b4c,
+	},
+
+	/* check if savegame is valid */
+	checkValidSavegame:function(){
+		return (tempFile.fileSize===262144)
+	},
+
+	/* preload function */
+	preload:function(){
+		setNumericRange('money', 0, 9999999);
+		setNumericRange('boughtpicrites', 0, 5000);
+	},
+
+	/* load function */
+	load:function(){
+		tempFile.fileName='DATA.DAT';
+
+		setValue('money', tempFile.readU16(this.Offsets.MONEY));
+		setValue('boughtpicrites', tempFile.readU16(this.Offsets.BOUGHTPICRITES));
+	},
+
+	/* save function */
+	save:function(){
+		tempFile.writeU16(this.Offsets.MONEY, getValue('money'));
+		var boughtPicrites=getValue('boughtpicrites');
+		tempFile.writeU16(this.Offsets.BOUGHTPICRITES, boughtPicrites);
+		var unlockedShopByte=tempFile.readU8(this.Offsets.UNLOCKEDSHOPFLAG) & ~0x01;
+		tempFile.writeU8(this.Offsets.UNLOCKEDSHOPFLAG, unlockedShopByte+(boughtPicrites>=5000)?0x01:0x00);
+	}
+}
